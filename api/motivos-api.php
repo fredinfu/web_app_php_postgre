f583c319f67f248667be41448f7dd7507f8decb5
Fredin Funez <fredinfu@live.com>
2019-06-18 20:51:56 -0600

    switch action api organization

--- api/motivos-api.php
+++ api/motivos-api.php
@@ -2,16 +2,38 @@
 
 	require "config.php";
 
-	$results = array();
-	$result = pg_query($con_postgres, "SELECT * FROM motivos_es_gt ");
 
-	if (!$result) {
-	  echo "An error occurred.\n";
-	  exit;
+	$action = isset($_GET["action"]) ? $_GET["action"] : (isset($_POST["action"]) ? $_POST["action"] : "");
+
+	switch ($action) {
+		case 'a1':
+			echo "An error occurred.\n";
+			break;
+		
+		case 'a2':
+			$results = array();
+			$result = pg_query($con_postgres, "SELECT * FROM motivos_es_gt ");
+
+			if (!$result) {
+			  echo "An error occurred.\n";
+			  exit;
+			}
+
+			while ($row = pg_fetch_array($result, null, PGSQL_ASSOC) ) {
+				$results[] = $row;
+			}
+
+			echo json_encode($results);
+			
+			break;
+
+		case 'a3':
+			echo "A3.\n";
+			break;
+
+
+		default:
+			# code...
+			break;
 	}
 
-	while ($row = pg_fetch_array($result, null, PGSQL_ASSOC) ) {
-		$results[] = $row;
-	}
-
-	echo json_encode($results);
