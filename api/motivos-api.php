2f38f1edf709bcc125b14f7b771e61a77dcf60f6
Fredin Funez <fredinfu@live.com>
2019-06-18 21:38:06 -0600

    CREATE operation motivo

--- api/motivos-api.php
+++ api/motivos-api.php
@@ -6,8 +6,34 @@
 	$action = isset($_GET["action"]) ? $_GET["action"] : (isset($_POST["action"]) ? $_POST["action"] : "");
 
 	switch ($action) {
-		case 'a1':
-			echo json_encode(array('success'=>false, 'message'=>'SERVER_ERROR'));
+		
+		case 'add_motivo':
+
+			$des_motivo = isset($_GET["des_motivo"]) 	? $_GET["des_motivo"] 	: (isset($_POST["des_motivo"]) 	? $_POST["des_motivo"] : "");
+			$estado 	= isset($_GET["estado"]) 		? $_GET["estado"] 		: (isset($_POST["estado"]) 		? $_POST["estado"] : "");
+			$tipo 		= isset($_GET["tipo"]) 			? $_GET["tipo"] 		: (isset($_POST["tipo"]) 		? $_POST["tipo"] : "");
+
+			$query = pg_query($con_postgres, "SELECT MAX(motivo)+1 AS motivo FROM motivos_es_gt ");
+			$max = pg_fetch_array($query, null, PGSQL_ASSOC);
+	
+
+			$query = "INSERT INTO motivos_es_gt VALUES (
+				 $max[motivo]
+				,'$des_motivo'
+				,'$estado'
+				,'$tipo'
+			)";
+
+			$result = pg_query($con_postgres, $query);
+
+			if (!$result) {
+			  echo json_encode(array('success'=>false, 'message'=>'SERVER_ERROR'));
+			  exit;
+			}
+
+			$result = json_encode(array('success'=>true, 'results'=>array("motivo" => $max['motivo'])));
+			echo $result;
+			
 			break;
 		
 		case 'get_motivos':
