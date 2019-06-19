5d266189ad37efaf827a7f7a2bb0da20cff43d84
Fredin Funez <fredinfu@live.com>
2019-06-18 23:44:18 -0600

    responsive angular material table components

--- app/src/app/motivos/motivos.page.ts
+++ app/src/app/motivos/motivos.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource, MatPaginator, MatSort, MatStep, MatStepper } from '@angular/material';
 
 @Component({
   selector: 'app-motivos',
@@ -7,9 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MotivosPage implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['motivo', 'des_motivo', 'estado', 'tipo'];
+
+  user: any;
+  ionite: any;
+  dummyData: any[] = [
+    {
+      motivo: 0,
+      des_motivo: 'ASDF',
+      estado: 'F',
+      tipo: 'T'
+    },
+    {
+      motivo: 1,
+      des_motivo: 'CSDF',
+      estado: 'V',
+      tipo: 'T'
+    },
+    {
+      motivo: 2,
+      des_motivo: 'GSDF',
+      estado: 'F',
+      tipo: 'K'
+    }
+  ];
+
+  dataSource = new MatTableDataSource<any>();
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor() {
+    this.getMotivos();
+  }
 
   ngOnInit() {
+    this.getMotivos();
+  }
+
+  getMotivos() {
+    this.dataSource.data = this.dummyData;
   }
 
 }
